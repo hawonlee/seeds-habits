0eb64d9516664f7bb06691df73279573ffb0a7b2
hawonlee <gracehawonlee@gmail.com>
2025-09-25 21:50:22 -0400

    dark mode theme toggle

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -116,7 +116,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-2 text-xs outline-none focus:bg-neutral-100 focus:text-accent-foreground data-[state=checked]:bg-muted data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-2 text-xs outline-none focus:bg-habitbg focus:text-accent-foreground data-[state=checked]:bg-habitbg data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
     {...props}
